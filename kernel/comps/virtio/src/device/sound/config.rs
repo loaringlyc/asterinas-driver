0761fa1e71b806894827522548d585eae32f64af
loaringlyc <3043119496@qq.com>
2024-12-11 19:42:20 +0800

    modify wrong in use packet

--- kernel/comps/virtio/src/device/sound/config.rs
+++ kernel/comps/virtio/src/device/sound/config.rs
@@ -1,9 +1,11 @@
 use aster_util::safe_ptr::SafePtr;
-use ostd::{offset_of, Pod};
+use ostd::Pod;
+use core::mem::offset_of;
 use crate::transport::{ConfigManager, VirtioTransport};
 bitflags::bitflags! {
     pub struct SoundFeatures: u64 {
-        const VIRTIO_SND_F_CTLS = 1 << 0; //Device supports control elements.
+        //Device supports control elements.
+        const VIRTIO_SND_F_CTLS = 1 << 0; 
     }
 }
 
@@ -30,17 +32,17 @@ impl ConfigManager<VirtioSoundConfig> {
     pub(super) fn read_config(&self, ctls_negotiated: bool) -> VirtioSoundConfig {
         let mut sound_config = VirtioSoundConfig::new_uninit();
         sound_config.jacks = self
-            .read_once::<u32>(offset_of!(VirtioSoundConfig, jacks) as usize)
+            .read_once::<u32>(offset_of!(VirtioSoundConfig, jacks))
             .unwrap_or(0);
         sound_config.streams = self
-            .read_once::<u32>(offset_of!(VirtioSoundConfig, streams) as usize)
+            .read_once::<u32>(offset_of!(VirtioSoundConfig, streams))
             .unwrap_or(0);
         sound_config.chmaps = self
-            .read_once::<u32>(offset_of!(VirtioSoundConfig, chmaps) as usize)
+            .read_once::<u32>(offset_of!(VirtioSoundConfig, chmaps))
             .unwrap_or(0);
         if ctls_negotiated {
             sound_config.controls = self
-                .read_once::<u32>(offset_of!(VirtioSoundConfig, controls) as usize)
+                .read_once::<u32>(offset_of!(VirtioSoundConfig, controls))
                 .unwrap_or(0);
         } else {
             sound_config.controls = 0;
